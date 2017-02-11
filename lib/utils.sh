608750b7b64030be93e000f2b07fda280a14d6e1
Jonathan Como <jonathan.como@gmail.com>
2017-02-11 00:10:30 -0800

    Execute build script in environment context

--- lib/utils.sh
+++ lib/utils.sh
@@ -9,3 +9,15 @@ read_env () {
     export "$name=$(cat $ENV_DIR/$name)"
   fi
 }
+
+export_env_dir() {
+  whitelist_regex=${1:-''}
+  blacklist_regex=${2:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+
+  if [ -d "$ENV_DIR" ]; then
+    for e in $(ls $ENV_DIR); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+        export "$e=$(cat $ENV_DIR/$e)"
+    done
+  fi
+}
